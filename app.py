c416f666079f3e05915a7538e52086d93af6b6d1
master <tsapko.dev@gmail.com>
2025-08-04 22:46:12 +0300

    start Flask

--- app.py
+++ app.py
@@ -2,7 +2,17 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask
+app = Flask(__name__)
 
+@app.route("/index")
+@app.route("/")
+def index():
+    return "<h1>Main str</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>Страница о нас</h1>"
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +21,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True)
+    print_hi('start my program App')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
